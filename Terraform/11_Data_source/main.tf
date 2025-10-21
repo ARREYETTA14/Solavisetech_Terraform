4ef5b40c4ae68da548c1daddf319b0218481de37
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2025-10-21 18:20:41 +0100

    Update main.tf

--- Terraform/11_Data_source/main.tf
+++ Terraform/11_Data_source/main.tf
@@ -1,21 +1,28 @@
 # # Below is the provider which helps in connecting with AWS Account
 # provider "aws" {
 #   region     = "sa-east-1"
+#   profile = "default"
 # }
 
-# # Below is the Data Sources Block
+# # # Below is the Data Sources Block
 # data "aws_ami" "app_ami" {
 #   most_recent = true
 #   owners = ["amazon"]
 
 #   filter {
 #     name   = "name"
-#     values = ["al2023-ami-2023.9.20250929.0-kernel-6.1-x86_64*"]
+#     values = ["al2023-ami-2023.*-kernel-6.1-x86_64"]
 #   }
+  
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+  
 # }
 
 # output "test" {
-#  value = data.aws_ami.app_ami
+#  value = data.aws_ami.app_ami.id
 # }
 
 # # advantageous
@@ -23,6 +30,10 @@
 #     ami = data.aws_ami.app_ami.id
 #    instance_type = "t2.micro"
 # }
+
+
+
+
 
 
 
