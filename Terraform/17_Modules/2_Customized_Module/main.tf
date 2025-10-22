3400675c5add79865a2ff8ce6255f0332ffacd79
Arreyetta Bawak Augustine Besong <112652000+ARREYETTA14@users.noreply.github.com>
2025-10-22 20:08:01 +0100

    Update main.tf

--- Terraform/17_Modules/2_Customized_Module/main.tf
+++ Terraform/17_Modules/2_Customized_Module/main.tf
@@ -6,10 +6,16 @@ module "my_vpc_module" {
 
 
 resource "aws_instance" "ec2" {
-  ami = "" //sa-east-1
-  instance_type = "t2.micro"
+  ami = "ami-0ba39aef11896824a" //sa-east-1
+  instance_type = "t3.micro"
   subnet_id = module.my_vpc_module.public_subnet_id
   tags = {
     Environment = "Test_Ec2"
   }
+}
+
+# Below is the provider which helps in connecting with AWS Account
+    provider "aws" {
+    region = "sa-east-1"
+    profile = "default"
 }
